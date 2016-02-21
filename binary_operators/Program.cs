4ba31759655f7cfb27a9b79ff09e661701da7693
AlexBennett <alexanderbondbennett@gmail.com>
2016-02-20 20:42:57 -0500

    working with binary operators

---
+++ binary_operators/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace binary_operators
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			int x, y, result;
+			float floatresult;
+
+			x = 7;
+			y = 5;
+
+			result = x + y;
+			Console.WriteLine ("x+y = {0}", result);
+
+			result = x - y;
+			Console.WriteLine ("x+y = {0}", result);
+
+		}
+	}
+}
